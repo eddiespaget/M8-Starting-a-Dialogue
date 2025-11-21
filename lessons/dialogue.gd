253c2489b72468224a8f4a28f4c3bbc21ce0eac6
eddiespaget <edsalas@s.sfusd.edu>
2025-11-21 15:47:06 -0800

    M8L5 halfway done
    continue at home

--- lessons/dialogue.gd
+++ lessons/dialogue.gd
@@ -52,13 +52,13 @@ var dialogue_items: Array[Dictionary] = [
 ]
 
 
-## UI element that shows the texts
+
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
-## Audio player that plays voice sounds while text is being written
+
 @onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
-## The character
+
 @onready var body: TextureRect = %Body
-## The Expression
+
 @onready var expression: TextureRect = %Expression
 @onready var action_buttons_v_box_container: VBoxContainer = %ActionButtonsVBoxContainer
 
@@ -67,43 +67,31 @@ func _ready() -> void:
 
 
 func show_text(current_item_index: int) -> void:
-	# We retrieve the current item from the array
+	
 	var current_item := dialogue_items[current_item_index]
-	# from the item, we extract the properties.
-	# We set the text to the rich text control
-	# And we set the appropriate expression texture
+
 	rich_text_label.text = current_item["text"]
 	expression.texture = current_item["expression"]
 	body.texture = current_item["character"]
 	create_buttons(current_item["choices"])
-	# We set the initial visible ratio to the text to 0, so we can change it in the tween
+	
 	rich_text_label.visible_ratio = 0.0
-	# We create a tween that will draw the text
+
 	var tween := create_tween()
-	# A variable that holds the amount of time for the text to show, in seconds
-	# We could write this directly in the tween call, but this is clearer.
-	# We will also use this for deciding on the sound length
+	
 	var text_appearing_duration: float = current_item["text"].length() / 30.0
-	# We show the text slowly
+
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
-	# We randomize the audio playback's start time to make it sound different
-	# every time.
-	# We obtain the last possible offset in the sound that we can start from
+	
 	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
-	# We pick a random position on that length
+	
 	var sound_start_position := randf() * sound_max_offset
-	# We start playing the sound
+
 	audio_stream_player.play(sound_start_position)
 	# We make sure the sound stops when the text finishes displaying
 	tween.finished.connect(audio_stream_player.stop)
 
-	# We animate the character sliding in.
 	slide_in()
-
-	# Finally, we disable the next button until the text finishes displaying.
-
-
-## Progresses to the next slide.
 
 
 func create_buttons(choices_data: Dictionary) -> void:
@@ -120,11 +108,6 @@ func create_buttons(choices_data: Dictionary) -> void:
 		else:
 			button.pressed.connect(show_text.bind(target_line_idx))
 
-## Draws the current text to the rich text element
-
-
-
-## Animates the character when they start talking
 func slide_in() -> void:
 	var slide_tween := create_tween()
 	slide_tween.set_ease(Tween.EASE_OUT)
